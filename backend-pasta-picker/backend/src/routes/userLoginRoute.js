6a427aef9df1bbe7a3d7cf7c93192e4d6a98d3ed
ranielcyrus <ranielcyrus97@gmail.com>
2024-10-11 16:15:28 +0800

    feat: add return error message

--- backend-pasta-picker/backend/src/routes/userLoginRoute.js
+++ backend-pasta-picker/backend/src/routes/userLoginRoute.js
@@ -54,7 +54,7 @@ router.post('/', async (req, res) => {
     } catch (error) {
       console.log(error)
       res.status(400).json({
-        message: 'bad request', error
+        message: `bad request ${error}` 
       })
     }
 })
